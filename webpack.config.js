1e565100562a50ef03705a4f94468f7f9982185f
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2018-10-13 23:29:02 +0100

    use template and clean before build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js'
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Test',
+      template: 'index.html'
+    })
+  ],
+
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
