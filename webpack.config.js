c27c77bf60c4b3709893826071a54a1c2fb44c77
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2018-10-13 23:58:20 +0100

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,15 @@ module.exports = {
   entry: {
     app: './src/index.js'
   },
-
+  resolve: {
+    alias: {
+      p5: 'p5/lib/p5.min.js'
+    }
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
@@ -14,7 +22,6 @@ module.exports = {
       template: 'index.html'
     })
   ],
-
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
