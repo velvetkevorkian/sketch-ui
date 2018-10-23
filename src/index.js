8be7ceac5b312813dd74f247f2e10222309ce7b0
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2018-10-24 00:06:06 +0100

    basic ui setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import p5 from 'p5'
 import './index.css'
 
+let state = {
+  fillColor: {
+    value: '#ff0000',
+    type: 'color'
+  },
+  backgroundColor: {
+    value: '#202020',
+    type: 'color'
+  }
+}
+
+function settings(key) {
+  return state[key].value
+}
+
+function createUI() {
+  const ui = document.getElementById('ui')
+
+  Object.entries(state).forEach(item => {
+    const name = item[0]
+    const config = item[1]
+
+    let label = document.createElement('label')
+    label.setAttribute('for', name)
+    label.appendChild(document.createTextNode(name))
+    ui.appendChild(label)
+
+    let input = document.createElement('input')
+    input.setAttribute('id', name)
+    input.setAttribute('type', config.type)
+    input.setAttribute('value', config.value)
+    input.addEventListener('input', event => {
+      state[name].value = event.target.value
+    })
+
+    ui.appendChild(input)
+  })
+}
+
 const sketch = p => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
+    createUI()
   }
 
   p.windowResized = () => {
@@ -11,8 +51,8 @@ const sketch = p => {
   }
 
   p.draw = () => {
-    p.background(20, 20, 20)
-    p.fill(255, 0, 0)
+    p.background(settings('backgroundColor'))
+    p.fill(settings('fillColor'))
     p.ellipse(p.windowWidth/2, p.windowHeight/2, 50, 50)
   }
 }
